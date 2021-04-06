29acc37d992abaab32ffe3bd7988e2dd3c6f2a2a
Aviv Elimelech <Aviv657@gmail.com>
2021-04-06 03:37:54 +0300

    Created a SignUp component and integrated firebase within

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,33 @@ const config = {
   appId: "1:883147329074:web:57bdc64afe98a9230db62b",
 };
 
-firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error){
+            console.log("error creating user ", error.message);
+        }
+    }
+    return userRef;
+
+}
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(config);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
