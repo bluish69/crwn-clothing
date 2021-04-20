f0c0b3434d8b40bfd11596245dfe66127c29309d
Aviv Elimelech <Aviv657@gmail.com>
2021-04-20 05:49:42 +0300

    Removed all the unneccessary files, this is the first working push

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import {combineReducers} from "redux";
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['cart', "user"]
+};
+
+const cartConfig = {
+    key: 'cart',
+    storage,
+    blacklist: ['hidden']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart : cartReducer
+    cart : persistReducer(cartConfig, cartReducer)
 })
+
+export default persistReducer(persistConfig, rootReducer);
